6965969510fe0b16c2746a03b1299cc4fe884fe6
Joshua Lopez <joshualopez0429@gmail.com>
2021-05-14 21:08:48 +0000

    Added API html link and status obj.

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,21 @@
+$( document ).ready(function () {
+  const amenityCheck = {};
+  $('.amenities INPUT').change(function() {
+    if (this.checked) {
+      amenityCheck.push($(this).attr('data-name'));
+    } else {
+      amenityCheck.pop($(this).attr('data-name'));
+    }
+    $('.amenities H4').html(Object.values(amenityCheck).join(', '));
+  });
+});
+
+$.get("http://0.0.0.0:5001/api/v1/status/", function(data) {
+  if (data.status === ("OK") {
+    $( "#api_status" ).addClass("available") {
+      else {
+    $( "#api_status" ).removeClass("available")
+      }
+    }
+  });
+});
