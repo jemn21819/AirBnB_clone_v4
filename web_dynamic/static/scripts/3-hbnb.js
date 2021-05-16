5fc1e28fbeca4439c88cbe6a73eefb2417f00387
James <jmartinez21834@stu.nuc.edu>
2021-05-16 18:12:57 +0000

    making changes in the file js fot task 4

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,6 +1,6 @@
 $( document ).ready(function () {
   const amenityCheck = {};
-  $('.amenities INPUT').change(function() {
+  $('.amenities INPUT').change(function () {
     if (this.checked) {
       amenityCheck.push($(this).attr('data-name'));
     } else {
@@ -8,30 +8,37 @@ $( document ).ready(function () {
     }
     $('.amenities H4').html(Object.values(amenityCheck).join(', '));
   });
-});
 
-$.get("http://0.0.0.0:5001/api/v1/status/", function(data) {
-  if (data.status === ("OK") {
-    $( "#api_status" ).addClass("available") {
-      else {
-    $( "#api_status" ).removeClass("available")
-      }
-    }
+  $.get("http://0.0.0.0:5001/api/v1/status/", function(data) {
+    if (data.status === "OK") {
+      $( "#api_status" ).addClass("available");
+    } else {
+      $( "#api_status" ).removeClass("available");
+        }
+  });
+
+  $.ajax({
+    data: '{}',
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search',
+    contentType: 'application/json,
+    dataType: 'json',
+    success: function (data) {
+      for (let x = 0; x < data.length; x++) {
+        let place = data[x];
+        $('.places').append('<article><h2>' + place.name +
+          '</h2><div class="price_by_night"><p> +
+          place.price_by_night +
+          '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' +
+          place.max_guest +
+          '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' +
+          place.number_rooms +
+          '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' +
+          place.number_bathrooms +
+          '</p></div></div><div class="description"><p>' +
+          place.description +
+          '</p></div></article>');
+	  }
+	}
   });
 });
-
-$.ajax({
-  data: '{}',
-  type: 'POST',
-  url: 'http://0.0.0.0:5001/api/v1/places_search',
-  contentType: 'application/json,
-  dataType: 'json',
-  success: function (data) {
-    for (let x = 0; x < data.length; x++) {
-      let place = data[x];
-      const guest = (value.max_guest > 1) ? 'guests' : 'guest';
-      const rooms = (value.number_rooms > 1) ? 'rooms' : 'room';
-      const bathrooms = (value.number_bathrooms > 1) ? 'bathrooms' : 'bathroom';
-      $
-
-
